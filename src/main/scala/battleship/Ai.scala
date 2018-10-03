59ea21ea040e2ee67a904ee8dc3ab910b8129566
Mohamed Iheb Faiza <fmohamediheb@gmail.com>
2018-10-03 20:32:58 +0200

    Add : working AI1 and AI2 with the Hunter heuristic

--- src/main/scala/battleship/Ai.scala
+++ src/main/scala/battleship/Ai.scala
@@ -6,7 +6,8 @@ import scala.util.Random
 case class Ai(primaryGrid: Grid,
               trackingGrid: Grid,
               fleet: List[Ship],
-              number: Int, level: Int)
+              number: Int, level: Int,
+              targets: Set[(Int, Int)]) //targets is a stack of potential targets we use sets to allow no duplicates but it allows no order so it can be problematic
   extends Player {
 
   val random = new Random()
@@ -15,31 +16,80 @@ case class Ai(primaryGrid: Grid,
     copy(primaryGrid, trackingGrid, fleet, number)
   }
 
-  override def askForTarget(): (Int, Int) = {
-    level match {
-      case 0 => (random.nextInt(10), random.nextInt(10)) //TODO this is not completely random
-      case _ => (0, 0)
+  def askForTarget(): (Int, Int) = {
+    var target = (random.nextInt(10), random.nextInt(10)) //TODO this is not completely random
+    if (level == 1) {
+      if (!trackingGrid.cells(target._1)(target._2).isEmpty){ // If we already shot this cell we try again
+        askForTarget() // TODO should also remove the target from the target list
+      }
     }
+    if (level == 2) {
+      if (targets.nonEmpty) {
+        target = targets.head
+      }
+      if (!trackingGrid.cells(target._1)(target._2).isEmpty){ // If we already shot this cell we try again
+        askForTarget() // TODO should also remove the target from the target list
+      }
+    }
+    target
   }
 
-  override def askForDirection(): Boolean = {
-    random.nextInt(2) match { // TODO Error because this gave -1 WTF
+  def askForDirection(): Boolean = {
+    random.nextInt(2) match {
       case 0 => false
       case _ => true
     }
   }
+
+  // Updates target grid and potential targets list
+  override def updateTracking(pos: (Int, Int), hit: Boolean): Ai = {
+    var newTargets : Set[(Int, Int)] = targets - pos
+    if (level == 2) {// TEST
+      println(newTargets)
+    }
+    if (hit) {
+      newTargets = cleanTargets(addTargets(newTargets, pos))
+      copy(trackingGrid = Grid(trackingGrid.cells.patch(pos._1, Seq(trackingGrid.cells(pos._1).patch(pos._2, Seq(trackingGrid.cells(pos._1)(pos._2).markHit), 1)), 1)), targets = newTargets)
+    }
+    else {
+      copy(trackingGrid = Grid(trackingGrid.cells.patch(pos._1, Seq(trackingGrid.cells(pos._1).patch(pos._2, Seq(trackingGrid.cells(pos._1)(pos._2).markMissed), 1)), 1)), targets = newTargets)
+    }
+  }
+
+  // pos must be in bounds
+  def addTargets(oldTargets: Set[(Int, Int)] ,pos: (Int, Int)): Set[(Int, Int)] = {
+    var newTargets = oldTargets
+    if (pos._1 - 1 >= 0) {
+      newTargets = newTargets + ((pos._1 - 1, pos._2))
+    }
+    if (pos._1 + 1 < Config.gridSize) {
+      newTargets = newTargets + ((pos._1 + 1, pos._2))
+    }
+    if (pos._2 - 1 >= 0) {
+      newTargets = newTargets + ((pos._1, pos._2 - 1))
+    }
+    if (pos._2 + 1 < Config.gridSize) {
+      newTargets = newTargets + ((pos._1, pos._2 + 1))
+    }
+    newTargets
+  }
+
+  def cleanTargets(oldTargets: Set[(Int,Int)]): Set[(Int,Int)] = {
+    oldTargets.filter{case (x, y) => trackingGrid.cells(x)(y).isEmpty}
+  }
+
 
   def printLevelWarning(): Unit = println("WARNING: Level unsupported")
 }
 
 object Ai {
 
-  def apply(fleet: List[Ship] = Nil, number: Int, level: Int = 0): Ai = {
+  def apply(fleet: List[Ship] = Nil, number: Int, level: Int = 0, targets: Set[(Int,Int)] = Set()): Ai = {
     if (fleet == Nil) {
-      new Ai(Grid(), Grid(), Nil, number, level)
+      new Ai(Grid(), Grid(), fleet, number, level, targets)
     }
     else {
-      new Ai(Grid().addFleet(fleet), Grid(), fleet, number, level)
+      new Ai(Grid().addFleet(fleet), Grid(), fleet, number, level, targets)
     }
   }
 }
