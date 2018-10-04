c79177715092fbe3ce51923584e493223281316a
Mohamed Iheb Faiza <fmohamediheb@gmail.com>
2018-10-04 22:25:53 +0200

    Add : render beautiful title

--- src/main/scala/battleship/Renderer.scala
+++ src/main/scala/battleship/Renderer.scala
@@ -1,8 +1,18 @@
 package battleship
 
-import battleship._
-
 class Renderer() {
+  def renderTitle(): Unit = {
+    clear()
+    println(Console.RED)
+    println("  ___       _   _   _        _    _      ")
+    println(" | _ ) __ _| |_| |_| |___ __| |_ (_)_ __                  __/___")
+    println(" | _ \\/ _` |  _|  _| / -_|_-< ' \\| | '_ \\           _____/______|")
+    println(" |___/\\__,_|\\__|\\__|_\\___/__/_||_|_| .__/   _______/_____\\_______\\_____")
+    println("  by Mohamed Iheb FAIZA            |_|      \\              < < <       |")
+    println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    println(Console.WHITE)
+  }
+
   def renderCell(c: Cell): Unit = {
     var sprite: String = c.status match {
       case 1 => Console.BLUE + "██" + Console.WHITE
